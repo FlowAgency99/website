85400d5b493a87b1b1218e4b09e509cb79add46b
FlowAgency99 <126727665+FlowAgency99@users.noreply.github.com>
2024-10-10 14:14:04 +0200

    Update page.js

--- src/app/legal-notice/page.js
+++ src/app/legal-notice/page.js
@@ -11,8 +11,8 @@ export default function LegalNotice() {
       <div className="legal-notice-container">
         <h1 className="section-title">Mentions Légales</h1> {/* Utilise une classe globale pour les titres */}
         <p className="section-description">
-          Conformément à l'article 6 de la loi n°2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé
-          aux utilisateurs du site www.flow-agency.digital l'identité des différents intervenants dans le cadre de sa réalisation et de
+          Conformément à l&apos;article 6 de la loi n°2004-575 du 21 juin 2004 pour la confiance dans l&apos;économie numérique, il est précisé
+          aux utilisateurs du site www.flow-agency.digital l&apos;identité des différents intervenants dans le cadre de sa réalisation et de
           son suivi :
         </p>
         
